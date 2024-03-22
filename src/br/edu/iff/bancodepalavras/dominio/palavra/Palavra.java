8149329c7f50410b1e5d091f8b219e838c1bd874
mariantoniafreitas <antonia.freitas2002@gmail.com>
2024-03-22 20:19:47 -0300

    Jogador e Palavra Factory

--- src/br/edu/iff/bancodepalavras/dominio/palavra/Palavra.java
+++ src/br/edu/iff/bancodepalavras/dominio/palavra/Palavra.java
@@ -3,128 +3,133 @@ package br.edu.iff.bancodepalavras.dominio.palavra;
 import java.util.Arrays;
 
 import br.edu.iff.bancodepalavras.dominio.letra.Letra;
+import br.edu.iff.bancodepalavras.dominio.letra.LetraFactory;
 import br.edu.iff.bancodepalavras.dominio.tema.Tema;
 import br.edu.iff.dominio.ObjetoDominioImpl;
 
-public class Palavra  extends ObjetoDominioImpl  {
+public class Palavra extends ObjetoDominioImpl {
 
 	private Tema tema;
-	private LetraFactory letraFactoy;
+	private static LetraFactory letraFactory;
 	private Letra encoberta;
 	private Letra[] palavra;
 	private int tamanhoDaPalavra;
-	
-	private Palavra(long id, String palavra , Tema tema) {
+
+	private Palavra(long id,
+			String palavra, Tema tema) {
 		super(id);
 		this.setTema(tema);
 		this.tamanhoDaPalavra = palavra.length();
 		this.setLetras(palavra);
 	}
 
-	//Criar e reconstruir precisam da implementação da camada de persistência
-	public Palavra criar(long id, String palavra , Tema tema) {
-		if(this.letraFactory==null) {
+	// Criar e reconstruir precisam da implementaÃ¯Â¿Â½Ã¯Â¿Â½o da camada de per
+	// istÃ¯Â¿Â½ncia
+	public static Palavra criar(long id, String palavra, Tema tema) {
+		if (letraFactory == null) {
 			throw new RuntimeException("Deve inicializar o letraFactory primeiro");
 		}
 		return new Palavra(id, palavra, tema);
 	}
-	public Palavra reconstruir(long id, String palavra , Tema tema) {
-		if(this.letraFactory==null) {
+
+	public static Palavra reconstruir(long id, String palavra, Tema tema) {
+		if (letraFactory == null) {
 			throw new RuntimeException("Deve inicializar o letraFactory primeiro");
 		}
 		return new Palavra(id, palavra, tema);
 	}
 
 	private void setTema(Tema tema) {
-		if(tema == null) {
-			throw new NullPointerException("O Tema não pode ser nulo.");
+		if (tema == null) {
+			throw new NullPointerException("O Tema nï¿½o pode ser nulo.");
 		}
 		this.tema = tema;
-		
+
 	}
-	
+
 	private void setLetras(String palavra) {
-		if(palavra == null || palavra.isBlank()) {
-			throw new NullPointerException("A palavra não pode ser nula nem vazia.");
+		if (palavra == null || palavra.isBlank()) {
+			throw new NullPointerException("A palavra nï¿½o pode ser nula nem vazia.");
 		}
 		this.palavra = new Letra[this.getTamanho()];
-		for(int i=0; i<= this.tamanhoDaPalavra-1; i++) {
-			//Precisa da implementação de LetraFactory letras[i] = getLetraFactory(palavra.charAt(i));
+		for (int i = 0; i <= this.tamanhoDaPalavra - 1; i++) {
+			// Precisa da implementaï¿½ï¿½o de LetraFactory letras[i] =
+			// getLetraFactory(palavra.charAt(i));
 		}
-		
+
 	}
-	
+
 	public Letra[] getLetras() {
 		Letra[] letrasTemp = Arrays.copyOf(this.palavra, this.palavra.length);
 		return letrasTemp;
 	}
-	
+
 	public Letra getLetra(int posicao) {
-		if(posicao == 32) {
-			throw new NullPointerException("A posição da letra palavra não pode vazia.");
+		if (posicao == 32) {
+			throw new NullPointerException("A posiï¿½ï¿½o da letra palavra nï¿½o pode vazia.");
 		}
 		return this.palavra[posicao];
 	}
-	
+
 	public void exibir(Object contexto, boolean[] posicoes) {
-		if(this.palavra==null) {
-			throw new RuntimeException("A palavra a ser exibida não pode ser nula.");
+		if (this.palavra == null) {
+			throw new RuntimeException("A palavra a ser exibida nï¿½o pode ser nula.");
 		}
-		for(int i = 0; i < this.getTamanho(); i++) {
-			if(posicoes[i] == true) {				
+		for (int i = 0; i < this.getTamanho(); i++) {
+			if (posicoes[i] == true) {
 				this.palavra[i].exibir(contexto);
-			}else {
+			} else {
 				this.encoberta.exibir(contexto);
 			}
 		}
 	}
-	
+
 	public int[] tentar(char codigo) {
-		if(this.palavra == null) {
+		if (this.palavra == null) {
 			throw new RuntimeException("Para tentar, a palavra deve ser inicializada.");
 		}
-		if(codigo == 32) {
-			throw new NullPointerException("O caracter não pode ser vazio.");
+		if (codigo == 32) {
+			throw new NullPointerException("O caracter nï¿½o pode ser vazio.");
 		}
 		int[] posicoesEncontrasDaLetra = new int[this.tamanhoDaPalavra];
-		for(int i=0; i <= this.getTamanho() ; i++) {
-			if(this.palavra[i].getCodigo() == codigo ) {
+		for (int i = 0; i <= this.getTamanho(); i++) {
+			if (this.palavra[i].getCodigo() == codigo) {
 				posicoesEncontrasDaLetra[i] = 1;
 			}
 		}
 		return posicoesEncontrasDaLetra;
 	}
-	
+
 	public Tema getTema() {
 		return this.tema;
 	}
-	
+
 	public boolean comparar(String palavra) {
-		if(this.palavra == null) {
+		if (this.palavra == null) {
 			throw new RuntimeException("Para comparar, a palavra deve ser inicializada.");
 		}
-		if(palavra == null || this.getTamanho() != palavra.length()) {
+		if (palavra == null || this.getTamanho() != palavra.length()) {
 			return false;
 		}
-		for(int i = 0; i <= this.tamanhoDaPalavra; i++) {
-			if(this.palavra[i].getCodigo() != palavra.charAt(i)) {
+		for (int i = 0; i <= this.tamanhoDaPalavra; i++) {
+			if (this.palavra[i].getCodigo() != palavra.charAt(i)) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	public int getTamanho() {
 		return this.tamanhoDaPalavra;
 	}
-	
+
 	@Override
 	public String toString() {
-		if(this.palavra == null) {
+		if (this.palavra == null) {
 			throw new RuntimeException("Para imprimir, a palavra deve ser inicializada.");
 		}
 		String palavraImpressao = null;
-		for(int i = 0; i <= this.getTamanho(); i++) {
+		for (int i = 0; i <= this.getTamanho(); i++) {
 			palavraImpressao = palavraImpressao + this.palavra[i];
 		}
 		return palavraImpressao;
---
+++ src/br/edu/iff/jogoforca/dominio/jogador/JogadorFactory.java
@@ -0,0 +1,7 @@
+package br.edu.iff.jogoforca.dominio.jogador;
+
+public interface JogadorFactory {
+
+    public Jogador getJogador(String nome);
+
+}
