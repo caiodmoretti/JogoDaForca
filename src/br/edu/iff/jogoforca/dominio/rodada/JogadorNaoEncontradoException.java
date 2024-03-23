d582623d71dc06e83d45b7bb978899d22771ed90
Caio <caiodmoretti@gmail.com>
2024-03-23 18:03:51 -0300

    Inclusão da classe RodadaAppService

---
+++ src/br/edu/iff/jogoforca/dominio/rodada/JogadorNaoEncontradoException.java
@@ -0,0 +1,15 @@
+package br.edu.iff.jogoforca.dominio.rodada;
+
+public class JogadorNaoEncontradoException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	private String jogador;
+	
+	public JogadorNaoEncontradoException(String jogador) {
+		this.jogador = jogador;
+	}
+
+	public String getJogador() {
+		return jogador;
+	}
+}
